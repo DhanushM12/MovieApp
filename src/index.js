10f9f263f031b1227fc910b83d322e8777e076c2
DhanushM12 <dhanushmukhi@gmail.com>
2020-06-05 22:47:32 +0530

    Created store, dispatched actions and subscribed to store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from "redux";
 import "./index.css";
 import App from "./components/App";
+import movies from "./reducers";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const store = createStore(movies);
+/*console.log("state", store);
+console.log("Before State", store.getState());
+
+// store.dispatch({
+//   type: "ADD_MOVIES",
+//   movies: [{ name: "Superman " }],
+// });
+
+console.log("After State", store.getState());*/
+
+ReactDOM.render(<App store={store} />, document.getElementById("root"));
