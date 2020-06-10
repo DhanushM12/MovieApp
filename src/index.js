5c1f407c8a7af247d694d2c5e5696b9bc793674d
DhanushM12 <dhanushmukhi@gmail.com>
2020-06-10 21:15:30 +0530

    combining reducers

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { createStore } from "redux";
-import "./index.css";
-import App from "./components/App";
-import movies from "./reducers";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import './index.css';
+import App from './components/App';
+import rootReducer from './reducers';
 
-const store = createStore(movies);
+const store = createStore(rootReducer);
 /*console.log("state", store);
 console.log("Before State", store.getState());
 
@@ -16,4 +16,4 @@ console.log("Before State", store.getState());
 
 console.log("After State", store.getState());*/
 
-ReactDOM.render(<App store={store} />, document.getElementById("root"));
+ReactDOM.render(<App store={store} />, document.getElementById('root'));
