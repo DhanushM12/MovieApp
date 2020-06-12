2fd217e216869c6e5e97003a83a8f22777e8713d
DhanushM12 <dhanushmukhi@gmail.com>
2020-06-12 13:03:34 +0530

    added middleware

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import './index.css';
 import App from './components/App';
 import rootReducer from './reducers';
 
-const store = createStore(rootReducer);
-/*console.log("state", store);
-console.log("Before State", store.getState());
+//function logger(obj, next, action)
+//logger(obj)(next)(action)
+/*const logger = function ({ dispatch, getState }) {
+  return function (next) {
+    return function (action) {
+      //middleware code
+      console.log('ACTION_TYPE = ', action.type);
+      next(action);
+    };
+  };
+};*/
+const logger = ({ dispatch, getState }) => (next) => (action) => {
+  //logger code
+  console.log('ACTION_TYPE = ', action.type);
+  next(action);
+};
+
+const store = createStore(rootReducer, applyMiddleware(logger));
+console.log('state', store);
+/*console.log("Before State", store.getState());
 
 // store.dispatch({
 //   type: "ADD_MOVIES",
